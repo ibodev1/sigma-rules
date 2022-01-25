24b3ae542bb9b640598ef13511a82c27c6c038d4
İbrahim Ödev <74017534+ibodev1@users.noreply.github.com>
2022-01-25 16:06:41 +0300

    ASD

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const data = require('./data');
 const ejs = require('ejs');
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.engine('.ejs', ejs.__express);
